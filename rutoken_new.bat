614415329aa003a16308fcf1a62252cb88743cf7
neraverin <neraverin@gmail.com>
2019-01-15 13:30:49 +0300

    Исправления для УТМ версии 3.0.8

--- rutoken_new.bat
+++ rutoken_new.bat
@@ -34,18 +34,11 @@ if not exist %system_root_cpp_style%\!sysarch!\rtPKCS11ECP.dll (
 copy /y %system_root_cpp_style%\!sysarch!\rtPKCS11ECP.dll %system_root_cpp_style%\!sysarch!\rtPKCS11ECP-replica.dll > nul
 %system_root_cpp_style%\!sysarch!\regsvr32.exe /s %system_root_cpp_style%\!sysarch!\rtPKCS11ECP-replica.dll > nul
 
-if not "!sysarch!" == "System32" (
-    echo Creating rtPKCS11ECP-replica.dll x86-64 version...
-    set sysarch=System32
-    set sysarch=!sysarch:~0,10!
-    copy /y %system_root_cpp_style%\!sysarch!\rtPKCS11ECP.dll %system_root_cpp_style%\!sysarch!\rtPKCS11ECP-replica.dll > nul
-    %system_root_cpp_style%\!sysarch!\regsvr32.exe /s %system_root_cpp_style%\!sysarch!\rtPKCS11ECP-replica.dll > nul
-)
-
 for %%i in (..\UTM\agent\conf\agent.properties
             ..\UTM\installer\conf\transport.properties
             ..\UTM\monitoring\conf\transport.properties
             ..\UTM\transporter\conf\transport.properties
+            ..\UTM\updater\conf\transport.properties
             ) do (
 
     set verfile=%%i
@@ -69,6 +62,33 @@ for %%i in (..\UTM\agent\conf\agent.properties
     call :patch_value !tempfile! !verfile! gost.library.path %system_root_cpp_style%\\!sysarch!\\rttranscrypt.dll
 )
 
+echo gost.pkcs11.library.path
+for %%i in (..\UTM\agent\conf\agent.properties
+            ..\UTM\installer\conf\transport.properties
+            ..\UTM\transporter\conf\transport.properties
+            ) do (
+
+    set verfile=%%i
+    set tempfile=!verfile!.tmp
+
+    for /F "tokens=1-3" %%i in ('find "gost.pkcs11.library.path" !verfile! /C') do set pkcs11_count=%%k
+    if "!pkcs11_count!" == "0" (
+        echo Adding gost.pkcs11.library.path to !verfile!
+        echo gost.pkcs11.library.path = %system_root_cpp_style%\\!sysarch!\\rtPKCS11ECP.dll >> !verfile!
+    ) else (
+        echo Fixing gost.pkcs11.library.path in !verfile!
+        call :patch_value_space !tempfile! !verfile! gost.pkcs11.library.path %system_root_cpp_style%\\!sysarch!\\rtPKCS11ECP.dll
+    )
+)
+
+if not "!sysarch!" == "System32" (
+    echo Creating rtPKCS11ECP-replica.dll x86-64 version...
+    set sysarch=System32
+    set sysarch=!sysarch:~0,10!
+    copy /y %system_root_cpp_style%\!sysarch!\rtPKCS11ECP.dll %system_root_cpp_style%\!sysarch!\rtPKCS11ECP-replica.dll > nul
+    %system_root_cpp_style%\!sysarch!\regsvr32.exe /s %system_root_cpp_style%\!sysarch!\rtPKCS11ECP-replica.dll > nul
+)            
+
 echo Success
 exit /b 0
 
@@ -76,6 +96,14 @@ exit /b 0
     for /f "delims=" %%a in (%2) do (
         (echo "%%a")|>nul find /i "%3="&&((echo %3=%4)>>%1)
         (echo "%%a")|>nul find /i "%3="||(echo %%a)>>%1
+    )
+    move /y %1 %2 >nul
+    exit /b 0
+
+:patch_value_space
+    for /f "delims=" %%a in (%2) do (
+        (echo "%%a")|>nul find /i "%3 ="&&((echo %3 = %4)>>%1)
+        (echo "%%a")|>nul find /i "%3 ="||(echo %%a)>>%1
     )
     move /y %1 %2 >nul
     exit /b 0
